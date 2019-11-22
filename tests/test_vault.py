b1bbb753f58a9b6d40c7db233fa6e596e5516396
Werner Dijkerman <werner@dj-wasabi.nl>
2019-11-22 17:56:51 +0100

    FIxing tests

--- tests/test_vault.py
+++ tests/test_vault.py
@@ -1,19 +1,19 @@
 import requests
 
-def test_socket(Socket):
-    assert Socket('tcp://0.0.0.0:8200').is_listening
+def test_socket(host):
+    assert host.socket('tcp://0.0.0.0:8200').is_listening
 
 
-def test_vault_config(File):
-    config_hcl = File("/vault/config/config.hcl")
+def test_vault_config(host):
+    config_hcl = host.file("/vault/config/config.hcl")
     assert config_hcl.user == "vault"
     assert config_hcl.contains('address = "0.0.0.0:8200"')
     assert config_hcl.contains('tls_disable = 1')
     assert config_hcl.contains('disable_mlock = false')
 
 
-def test_users(User):
-    user = User("vault")
+def test_users(host):
+    user = host.user("vault")
     assert user.exists
 
 
